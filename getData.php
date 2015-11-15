41daaf9267541490af7173f9438e793f05b95a73
streetcoder <proshimul@yahoo.com>
2015-11-16 01:04:44 +0600

    init...

---
+++ getData.php
@@ -0,0 +1,15 @@
+<?php
+
+$followers =
+    array(
+        array('x'=>1, 'y'=>5),
+        array('x'=>2, 'y'=>30),
+        array('x'=>3, 'y'=>80),
+        array('x'=>4, 'y'=>140),
+        array('x'=>5, 'y'=>160),
+        array('x'=>6, 'y'=>200),
+        array('x'=>7, 'y'=>250),
+        array('x'=>8, 'y'=>260),
+);
+
+echo json_encode($followers);
